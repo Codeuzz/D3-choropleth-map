a957fa43cce7703621d7f2212c67656f53877658
Codeuzz <belinda.zilevu@gmail.com>
2024-10-09 14:17:14 +0200

    finished county file, need to do education

--- main.js
+++ main.js
@@ -1,24 +1,43 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as d3 from "d3";
 
 document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+  <div id="container">
+    <div id="text">
+      <h1 id="title">United States Educational Attainment</h1>
+      <h3 id="description">Percentage of adults age 25 and older with a bachelor's degree or higher (2010-2014)</h3>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
   </div>
 `
 
-setupCounter(document.querySelector('#counter'))
+const EDUCATION_FILE =
+  'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json';
+const COUNTY_FILE =
+  'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json';
+
+d3.json(COUNTY_FILE)
+  .then(data => makeMap(data))
+  .catch(err => console.log(err));
+
+const makeMap = (bb) => {
+  console.log(bb)
+
+  let width = 960, height = 600;
+  let path = d3.geoPath()
+
+  let svg = d3.select("#container").append('svg')
+    .style("width", width).style("height", height);
+
+  svg.append('g')
+    .attr('class', 'counties')
+    .selectAll('path')
+    .data(topojson.feature(bb, bb.objects.counties).features)
+    .enter()
+    .append('path')
+    .attr('d', path)
+    .attr('fill', '#088')
+    .attr('stroke', '#000')
+    .attr('class', 'county')
+}
+  
+
